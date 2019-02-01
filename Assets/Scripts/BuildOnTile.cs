abd6cdad515609baaf9189627b8f6ab023cb3317
syy1125 <syy1125@hotmail.com>
2019-02-01 00:24:04 -0800

    Working isometric tilemap raycast

---
+++ Assets/Scripts/BuildOnTile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BuildOnTile : MonoBehaviour
+{
+	public Tilemap Map;
+
+	private Camera _mainCamera;
+	private Plane _zPlane;
+
+	private void Start()
+	{
+		_mainCamera = Camera.main;
+		_zPlane = new Plane(Vector3.forward, 0);
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray mouseRay = _mainCamera.ScreenPointToRay(Input.mousePosition);
+			if (!_zPlane.Raycast(mouseRay, out float distance))
+			{
+				Debug.LogError("Failed to raycast onto Z plane.");
+				return;
+			}
+			
+			Vector3Int tilePosition = Map.WorldToCell(mouseRay.GetPoint(distance));
+			Debug.Log(mouseRay.GetPoint(10 * mouseRay.direction.magnitude / mouseRay.direction.z));
+			Debug.Log(tilePosition);
+		}
+	}
+}
