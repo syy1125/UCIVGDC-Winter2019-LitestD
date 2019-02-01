8313ce5a87d798c3b4d4e39baf983019531df7e6
syy1125 <syy1125@hotmail.com>
2019-01-31 22:50:52 -0800

    Implement GameManager script placeholder

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance;
+	
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			Debug.LogWarning("A duplicate instance of GameManager is attempting to initialize. Destroying it.");
+			Destroy(gameObject);
+		}
+	}
+}
