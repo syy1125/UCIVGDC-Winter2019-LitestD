464da0622a11f30ce73e51fe8a82392a4ca58b7d
syy1125 <syy1125@hotmail.com>
2019-02-02 16:03:55 -0800

    Implement housing capacity using IntReference system

--- Assets/Scripts/BuildingTiles/HousingProvider.cs
+++ Assets/Scripts/BuildingTiles/HousingProvider.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Housing : MonoBehaviour
+public class HousingProvider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public IntReference HousingCapacity;
+	public int Capacity;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void OnEnable()
+	{
+		HousingCapacity.value += Capacity;
+	}
+
+	private void OnDisable()
+	{
+		HousingCapacity.value -= Capacity;
+	}
 }
