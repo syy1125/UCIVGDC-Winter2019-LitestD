48bbf3c98d2ba7d46109a6e2ecc9072b2d8abe6c
syy1125 <syy1125@hotmail.com>
2019-02-02 16:58:25 -0800

    Generalize capacity provider
    All the capacity providers do similar things, which is increasing an `IntegerRef` by a certain amount when enabled.

---
+++ Assets/Scripts/BuildingTiles/CapacityProvider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CapacityProvider : MonoBehaviour
+{
+	public IntReference CapacityRef;
+	public int CapacityDelta;
+
+	private void OnEnable()
+	{
+		CapacityRef.value += CapacityDelta;
+	}
+
+	private void OnDisable()
+	{
+		CapacityRef.value -= CapacityDelta;
+	}
+}
