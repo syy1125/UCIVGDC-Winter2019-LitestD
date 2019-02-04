4fe18127999984e8494a25c3ca4bd684a3afd5ef
syy1125 <syy1125@hotmail.com>
2019-02-04 13:30:10 -0800

    Begin implementing resource management

---
+++ Assets/Scripts/GameManagers/ResourceManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+	[Header("Capacity References")]
+	public IntReference HousingCapacity;
+	public IntReference GeneratorCapacity;
+	public IntReference FarmCapacity;
+
+	[Header("Balancing")]
+	public int PowerPerTechnician = 3;
+	public int FoodPerFarmer = 3;
+
+	[HideInInspector]
+	public int Population;
+	[HideInInspector]
+	public int GeneratorWorkerCount;
+	[HideInInspector]
+	public int FarmWorkerCount;
+
+}
