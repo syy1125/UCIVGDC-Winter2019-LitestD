0c8ca54e452294c321c0ad4da6e3ba83fcecdfe5
syy1125 <syy1125@hotmail.com>
2019-02-06 16:12:03 -0800

    Resource manager now uses more int reference

--- Assets/Scripts/GameManagers/ResourceManager.cs
+++ Assets/Scripts/GameManagers/ResourceManager.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResourceManager : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class ResourceManager : MonoBehaviour
 	public IntReference HousingCapacity;
 	public IntReference GeneratorCapacity;
 	public IntReference FarmCapacity;
+	[Space]
+	public IntReference Population;
+	public IntReference GeneratorWorkerCount;
+	public IntReference FarmWorkerCount;
 
 	[HideInInspector]
 	public HashSet<PowerConsumer> PowerConsumers;
@@ -16,10 +21,32 @@ public class ResourceManager : MonoBehaviour
 	public int PowerPerTechnician = 3;
 	public int FoodPerFarmer = 3;
 
-	[HideInInspector]
-	public int Population;
-	[HideInInspector]
-	public int GeneratorWorkerCount;
-	[HideInInspector]
-	public int FarmWorkerCount;
+
+	[Header("Rendering")]
+	public GameEvent UpdateUIEvent;
+	public TextMeshProUGUI HousingDisplay;
+	public TextMeshProUGUI FreeWorkforceDisplay;
+	public TextMeshProUGUI GeneratorDisplay;
+	public TextMeshProUGUI FarmDisplay;
+	public Button AssignGeneratorButton;
+	public Button UnassignGeneratorButton;
+	public Button AssignFarmButton;
+	public Button UnassignFarmButton;
+	
+	public void Display()
+	{
+		HousingDisplay.text =
+			$"Housing {Population} / {HousingCapacity} ({HousingCapacity - Population} free)";
+		FreeWorkforceDisplay.text =
+			$"Unassigned: {Population - GeneratorWorkerCount - FarmWorkerCount}";
+		GeneratorDisplay.text =
+			$"Generator: {GeneratorWorkerCount} / {GeneratorCapacity} (+{PowerPerTechnician * GeneratorWorkerCount} power)";
+		FarmDisplay.text =
+			$"Farm: {FarmWorkerCount} / {FarmCapacity.value} (+{FoodPerFarmer * FarmWorkerCount} food)";
+
+		AssignGeneratorButton.interactable = GeneratorWorkerCount < GeneratorCapacity;
+		UnassignGeneratorButton.interactable = GeneratorWorkerCount > 0;
+		AssignFarmButton.interactable = FarmWorkerCount < FarmCapacity;
+		UnassignFarmButton.interactable = FarmWorkerCount > 0;
+	}
 }
