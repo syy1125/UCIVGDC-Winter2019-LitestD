9b4db36edabae782e3f47ef06ee14a56580848ca
syy1125 <syy1125@hotmail.com>
2019-02-09 17:52:55 -0800

    Implement ground pathfinding data storage

---
+++ Assets/Scripts/ReferenceScripts/EnemyPathfinding.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.Tilemaps;
+
+[Serializable]
+public struct TileAttraction
+{
+	public TileBase Tile;
+	public float Attraction;
+}
+
+[CreateAssetMenu(menuName = "Config/Enemy Pathfinding")]
+public class EnemyPathfinding : ScriptableObject
+{
+	public TileAttraction[] TileAttractions;
+	public AnimationCurve FalloffCurve;
+	public int MaxRadius = 16;
+
+	// An attraction map works similar to potential energy, except negative.
+	// The higher the attraction value, the more preferable that tile is - either for attacking or for moving into.
+	public Dictionary<Vector3Int, float> MakeAttractionMap(Tilemap buildingMap, IEnumerable<Vector3Int> targetPositions)
+	{
+		DateTime startTime = DateTime.Now;
+		Dictionary<TileBase, float> tileAttractionValues = TileAttractions.ToDictionary(
+			tileAttraction => tileAttraction.Tile,
+			tileAttraction => tileAttraction.Attraction
+		);
+
+		var attractionMap = new Dictionary<Vector3Int, float>();
+
+		foreach (Vector3Int targetPosition in targetPositions)
+		{
+			float attraction = 0;
+			foreach (Vector3Int buildingPosition in buildingMap.cellBounds.allPositionsWithin)
+			{
+				if (!buildingMap.HasTile(buildingPosition)) continue;
+				TileBase buildingTile = buildingMap.GetTile(buildingPosition);
+
+				int distance = Mathf.Abs(buildingPosition.x - targetPosition.x)
+				               + Mathf.Abs(buildingPosition.y - targetPosition.y);
+				float weightedBuildingAttraction = tileAttractionValues[buildingTile]
+				                                   * FalloffCurve.Evaluate((float) distance / MaxRadius);
+				attraction += weightedBuildingAttraction;
+			}
+
+			attractionMap.Add(targetPosition, attraction);
+		}
+
+		Debug.Log($"Attraction map made in {(DateTime.Now - startTime).TotalMilliseconds} milliseconds");
+
+		return attractionMap;
+	}
+}
