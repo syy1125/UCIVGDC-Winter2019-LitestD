eccee0160e6985a2f04cffd643df966a0e0573dd
syy1125 <syy1125@hotmail.com>
2019-02-11 13:33:47 -0800

    Convert build orders to queue
    Issued build orders are now stored in a queue.
    The orders are executed one per turn.

--- Assets/Scripts/UI/BuildOnTile.cs
+++ Assets/Scripts/UI/BuildOnTile.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
@@ -15,35 +16,46 @@ public class BuildOnTile : MonoBehaviour
 
 	public TileBase SelectedTile { get; private set; }
 
+	private List<Tuple<Vector3Int, TileBase>> _buildingQueue;
+
 	private void Start()
 	{
 		_mainCamera = Camera.main;
+		_buildingQueue = new List<Tuple<Vector3Int, TileBase>>();
 	}
 
 	private void Update()
 	{
-		if (Input.GetMouseButtonDown(0))
+		if (!Input.GetMouseButtonDown(0)) return;
+		
+		Ray mouseRay = _mainCamera.ScreenPointToRay(Input.mousePosition);
+		if (!_zPlane.Raycast(mouseRay, out float distance))
 		{
-			Ray mouseRay = _mainCamera.ScreenPointToRay(Input.mousePosition);
-			if (!_zPlane.Raycast(mouseRay, out float distance))
-			{
-				Debug.LogError("Failed to raycast onto Z plane.");
-				return;
-			}
-
-			Vector3Int tilePosition = GroundMap.WorldToCell(mouseRay.GetPoint(distance));
-			// TODO Implement better placement logic
-			if (GroundMap.HasTile(tilePosition))
-			{
-				BuildingMap.SetTile(tilePosition, SelectedTile);
-				UpdateUIEvent.Raise();
-			}
+			Debug.LogError("Failed to raycast onto Z plane.");
+			return;
 		}
+
+		Vector3Int tilePosition = GroundMap.WorldToCell(mouseRay.GetPoint(distance));
+		if (!GroundMap.HasTile(tilePosition)) return;
+			
+		_buildingQueue.Add(new Tuple<Vector3Int, TileBase>(tilePosition, SelectedTile));
+		Debug.Log($"Building queue has {_buildingQueue.Count} members.");
 	}
 
 	public void SelectBuildTile(TileBase tile)
 	{
 		SelectedTile = tile;
 		SelectBuildTileEvent.Raise();
+	}
+
+	public void ExecuteBuildOrder()
+	{
+		if (_buildingQueue.Count <= 0) return;
+		
+		(Vector3Int tilePosition, TileBase selectedTile) = _buildingQueue[0];
+		_buildingQueue.RemoveAt(0);
+		BuildingMap.SetTile(tilePosition, selectedTile);
+		UpdateUIEvent.Raise();
+		Debug.Log($"Building queue has {_buildingQueue.Count} members.");
 	}
 }
