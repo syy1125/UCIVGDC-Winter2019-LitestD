8de48713800ce8359fd2c38e4e452572c486f402
syy1125 <syy1125@hotmail.com>
2019-02-26 00:01:46 -0800

    Implement pointer event adaptor

---
+++ Assets/Scripts/PointerEventAdaptor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[Serializable]
+public class PointerEvent : UnityEvent<PointerEventData>
+{}
+
+public class PointerEventAdaptor : MonoBehaviour,
+	IPointerClickHandler,
+	IPointerUpHandler,
+	IPointerDownHandler,
+	IPointerEnterHandler,
+	IPointerExitHandler
+{
+	public PointerEvent PointerClick;
+	public PointerEvent PointerUp;
+	public PointerEvent PointerDown;
+	public PointerEvent PointerEnter;
+	public PointerEvent PointerExit;
+	
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		PointerClick.Invoke(eventData);
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		PointerUp.Invoke(eventData);
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		PointerDown.Invoke(eventData);
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		PointerEnter.Invoke(eventData);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		PointerExit.Invoke(eventData);
+	}
+}
