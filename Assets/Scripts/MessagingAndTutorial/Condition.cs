b51e7f105c3d9d469306227f0c08ab17c98d4374
syy1125 <syy1125@hotmail.com>
2019-03-13 14:50:23 -0700

    Move default return of condition to default branch of switch

--- Assets/Scripts/MessagingAndTutorial/Condition.cs
+++ Assets/Scripts/MessagingAndTutorial/Condition.cs
@@ -1,38 +1,40 @@
-﻿[System.Serializable]
+﻿using System;
+
+[System.Serializable]
 public class Condition
 {
-    public enum ConditionType
-    {
-        Equals,
-        NotEquals,
-        LessThan,
-        LessThanOrEqual,
-        GreaterThan,
-        GreaterThanOrEqual
-    }
+	public enum ConditionType
+	{
+		Equals,
+		NotEquals,
+		LessThan,
+		LessThanOrEqual,
+		GreaterThan,
+		GreaterThanOrEqual
+	}
 
-    public IntReference intReference;
-    public ConditionType conditionType;
-    public int value;
+	public IntReference intReference;
+	public ConditionType conditionType;
+	public int value;
 
-    public bool IsMet()
-    {
-        switch (conditionType)
-        {
-            case ConditionType.Equals:
-                return intReference == value;
-            case ConditionType.NotEquals:
-                return intReference != value;
-            case ConditionType.LessThan:
-                return intReference < value;
-            case ConditionType.LessThanOrEqual:
-                return intReference <= value;
-            case ConditionType.GreaterThan:
-                return intReference > value;
-            case ConditionType.GreaterThanOrEqual:
-                return intReference >= value;
-        }
-
-        return false;
-    }
+	public bool IsMet()
+	{
+		switch (conditionType)
+		{
+			case ConditionType.Equals:
+				return intReference == value;
+			case ConditionType.NotEquals:
+				return intReference != value;
+			case ConditionType.LessThan:
+				return intReference < value;
+			case ConditionType.LessThanOrEqual:
+				return intReference <= value;
+			case ConditionType.GreaterThan:
+				return intReference > value;
+			case ConditionType.GreaterThanOrEqual:
+				return intReference >= value;
+			default:
+				return false;
+		}
+	}
 }
